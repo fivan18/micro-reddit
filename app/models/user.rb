37de52cf3f2ca8cfbd34ac384bfb0bd3db94cf7a
certilremy <certilremy@gmail.com>
2020-02-28 12:22:54 -0500

    create the user model

---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ApplicationRecord
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 6, maximum: 16 }
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 4, maximum: 12 }
+end
